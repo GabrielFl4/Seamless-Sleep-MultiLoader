db4cd6bd6f663a77247e5965c1698e4ff500af37
AquaLoco <gabrielmorandim05@gmail.com>
2026-02-25 03:52:03 -0300

    1.21.9 and Neoforge fix

--- fabric/src/main/java/net/aqualoco/sec/SeamlessSleepClient.java
+++ fabric/src/main/java/net/aqualoco/sec/SeamlessSleepClient.java
@@ -5,7 +5,7 @@ import net.aqualoco.sec.client.SleepStatusOverlay;
 import net.aqualoco.sec.network.SleepAnimationNetworking;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.world.level.Level;
 
@@ -16,26 +16,20 @@ public class SeamlessSleepClient implements ClientModInitializer {
     public void onInitializeClient() {
         SleepAnimationNetworking.initClient();
 
-        WorldRenderEvents.START.register(context -> {
-            ClientLevel world = context.world();
-            if (world == null) {
-                return;
-            }
-
-            if (!world.dimension().equals(Level.OVERWORLD)) {
-                return;
-            }
-
-            if (SeamlessSleepClientState.SLEEP_ANIMATION.isActive()) {
-                SeamlessSleepClientState.SLEEP_ANIMATION.tick(world);
-            }
-        });
-
         HudRenderCallback.EVENT.register(
-                (graphics, tickDelta) -> SleepStatusOverlay.render(
-                        graphics,
-                        SeamlessSleepClientState.SLEEP_ANIMATION
-                )
+                (graphics, tickDelta) -> {
+                    ClientLevel world = Minecraft.getInstance().level;
+                    if (world != null
+                            && world.dimension().equals(Level.OVERWORLD)
+                            && SeamlessSleepClientState.SLEEP_ANIMATION.isActive()) {
+                        SeamlessSleepClientState.SLEEP_ANIMATION.tick(world);
+                    }
+
+                    SleepStatusOverlay.render(
+                            graphics,
+                            SeamlessSleepClientState.SLEEP_ANIMATION
+                    );
+                }
         );
     }
 }
